6342da8f203062c2e7f1a73f2efd947a1b20c22e
Meshal Al-zahrni <sh3lool@hotmail.com>
2019-02-10 13:43:47 +0300

    Mohammed Aljumiah , Mishal

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from restaurants import templates 
 
 # Create your views here.
+def task_02 (request):
+	context= {
+	 	  'msg' : "Hello World!" ,
+
+
+
+	}
+	return render(request, 'index.html', context)
